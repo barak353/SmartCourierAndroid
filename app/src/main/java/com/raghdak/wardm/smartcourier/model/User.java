351633e7589c085b9138ccaf4bbddc900ae26f6e
Barak Segal <barak353@gmail.com>
2019-02-14 19:49:58 +0200

    Some minor fixes

--- app/src/main/java/com/raghdak/wardm/smartcourier/model/User.java
+++ app/src/main/java/com/raghdak/wardm/smartcourier/model/User.java
@@ -12,7 +12,7 @@ public class User implements Serializable {
     private String password;
     private String token;
     public static User currentUser;
-    public final static String ip = "f7a8eca9.ngrok.io";//Copy the URL from forwarding line (Remove the: http://  !!!).
+    public final static String ip = "localhost:8080";//Copy the URL from forwarding line (Remove the: http://  !!!).
 
     public User(String email, String password) {
         super();
